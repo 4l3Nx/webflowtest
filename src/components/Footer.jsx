07ea55c70950e4a7d2df771ac879f3484ba4c8af
Anonymus87! <alenmelkicx@gmail.com>
2021-01-20 13:10:09 +0000

    Webflow app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FooterStyle from './FooterStyle.css'
+
+const Footer = () => {
+    return (
+        <div>
+            <h1>This is Webflow Footer</h1>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeaderStyle from './HeaderStyle.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h1>This is Webflow Header</h1>
+        </div>
+        )
+}
+
+export default Header
